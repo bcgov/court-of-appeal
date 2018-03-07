0de7b44ad970c579d23331af814a8748848f2737
ericminio <eric.mignot@gmail.com>
2018-03-07 10:23:43 -0800

    We now display E-filing header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div id="header" role="banner">
+          <div id="header-main" class="navbar navbar-default navbar-fixed-top">
+              <div class="container">
+                  <div id="header-main-row" class="row">
+                      <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2 header-main-left">
+                          <div id="logo">
+                              <a href="http://gov.bc.ca"><img src="/images/gov3_bc_logo.png" alt="Province of British Columbia" title="Province of British Columbia logo" /></a>
+                          </div>
+                      </div>
+                      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                          <div class="bcgov-title">
+                              <h1>E-Filing</h1>
+                          </div>
+                      </div>
+                      <div class="col-xs-6 col-sm-6 col-md-7 col-lg-7">
+                              <a id="logout" class="pull-right" href="/logout" style={{color:'white',whiteSpace:'nowrap',marginTop:'30px'}}>Log out</a>
+                      </div>                  
+                  </div>
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
