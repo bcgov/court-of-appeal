375b69925bccf443968b23c774c49157fcc02163
Eric Mignot <eric.mignot@gmail.com>
2018-03-07 17:59:33 -0800

    forms moved in their folder

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 import Dashboard from './Dashboard.js';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Form7 from './Form.7.js';
-import Form2 from './Form.2.js';
+import Form7 from './forms/Form.7.js';
+import Form2 from './forms/Form.2.js';
 
 class App extends Component {
   render() {
