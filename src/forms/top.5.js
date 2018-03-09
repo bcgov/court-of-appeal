39a6421f8bbcfe76e3c87081416f8be31f1407c1
Eric Mignot <eric.mignot@gmail.com>
2018-03-09 10:12:00 -0800

    First top-5 section in Dashboard

---
+++ src/forms/top.5.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './top.5.css';
+
+class Top5 extends Component {
+
+    render() {
+        return (
+            <div id="top-5">
+                <h3>Top 5 Most Used Forms</h3>
+                <ol>
+                    <li><a id="form7" href="/forms/form.7.html" target="_self">Notice of Appeal (Form 7)</a></li>
+                    <li><a id="form2" href="/forms/form.2.html" target="_self">Notice of Appearance (Form 2)</a>&nbsp;</li>
+                    <li>Notice of Application for Leave to Appeal (Form 1)</li>
+                    <li></li>
+                    <li></li>
+                </ol>
+            </div>
+        )
+    }
+}
+export default Top5;
