1983a9dee8a412161459e77f7c0149aa361250ff
Eric Mignot <eric.mignot@gmail.com>
2018-03-09 09:50:07 -0800

    We now have a SelectForm component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,48 +1,14 @@
 import React, { Component } from 'react';
+import SelectForm from './forms/select.form.js';
 
 class Dashboard extends Component {
   render() {
     return (
         <div id="topicTemplate" className="template container gov-container">
-
             <div className="row">
-
-                <div id="main-content" role="main" className="contentPageMainColumn col-sm-12">
-                
-                    <div id="body">
-                        <h2>COURT OF APPEAL FORMS</h2>
-                        <h3>
-                        <span style={{ fontWeight:'bold' }}>DOCUMENTS TO BE USED BY THE APPELANT</span> <span style={{ color:'gray' }}>(Use these documents if you are STARTING the appeal)</span>
-                        </h3>
-
-                        <h4>Right to Appeal <span style={{ color:'gray' }}>(Start appeal without court's permission)</span></h4>
-                        <ul>
-                            <li><a id="form7" href="/forms/form.7.html" target="_self">Notice of Appeal (Form 7)</a></li>
-                            <li>Affidavit of Service <i className="far fa-file"></i><i className="far fa-file-pdf"></i></li>
-                        </ul>
-
-                        <h4>Leave to Appeal <span style={{ color:'gray' }}>(with court's permission)</span></h4>
-                        <ul>
-                            <li>Notice of Application for Leave to Appeal (Form 1)</li>
-                        </ul>
-
-                        <h3>
-                        <span style={{fontWeight:'bold' }}>DOCUMENTS TO BE USED BY THE RESPONDENT</span> <span style={{ color:'gray' }}>(Use these documents if you are STARTING the appeal)</span>
-                        </h3>
-
-                        <h4>Right to Appeal <span style={{ color:'gray' }}>(Start appeal without court's permission)</span></h4>
-                        <ul>
-                            <li><a id="form2" href="/forms/form.2.html" target="_self">Notice of Appearance (Form 2)</a>&nbsp;</li>
-                        </ul>
-
-                        <h4>Leave to Appeal <span style={{ color:'gray' }}>(with court's permission)</span></h4>
-                        <ul>
-                            <li><a id="form2" href="/forms/form.2.html" target="_self">Notice of Appearance (Form 2)</a>&nbsp;</li>
-                        </ul>
-                    </div>
+                <div id="main-content" role="main" className="contentPageMainColumn col-sm-12">                
+                    <SelectForm />
                 </div>
-
-
             </div>
         </div>
     );
---
+++ src/forms/select.form.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './select.form.css';
+
+class SelectForm extends Component {
+
+    render() {
+        return (
+            <div id="select-form">
+                <h2>START E-FILING</h2>
+                Are you starting an Appel?
+
+                <div id="steps" className="small-steps">
+                    <ol>
+                        <li className="small-steps"><span className="step-circle in-progress small-steps">1</span><span className="step-title in-progress small-steps">Select form</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">2</span><span className="step-title future small-steps">Complete form</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">3</span><span className="step-title future small-steps">Pay</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">4</span><span className="step-title future small-steps">Submit</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">5</span><span className="step-title future small-steps">Notfications</span></li>
+                    </ol>
+                </div>
+                
+                <table id="forms-table">
+                    <tr>
+                        <td>Are you starting an Appeal?</td>
+                        <td className="left-bordered">Are you responding to Appeal?</td>
+                    </tr>
+                    <tr>
+                        <td>COA FORMS FOR APPELANTS</td>
+                        <td className="left-bordered">CAO FORMS FOR RESPONDANTS</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div>
+                                <table><tr>
+                                    <td>
+                                        Right to Appeal <span style={{ color:'gray' }}>(Start appeal without court's permission)</span>
+                                        <ul>
+                                            <li><a id="form7" href="/forms/form.7.html" target="_self">Notice of Appeal (Form 7)</a></li>
+                                            <li>Affidavit of Service <i className="far fa-file"></i><i className="far fa-file-pdf"></i></li>
+                                        </ul>
+                                    </td>
+                                    <td>
+                                        Leave to Appeal <span style={{ color:'gray' }}>(with court's permission)</span>
+                                        <ul>
+                                            <li>Notice of Application for Leave to Appeal (Form 1)</li>
+                                        </ul>
+                                    </td>
+                                </tr></table>
+                            </div>
+                        </td>
+                        <td className="left-bordered">
+                            <div>
+                                <table><tr>
+                                    <td>
+                                        Right to Appeal <span style={{ color:'gray' }}>(Start appeal without court's permission)</span>
+                                        <ul>
+                                            <li><a id="form2" href="/forms/form.2.html" target="_self">Notice of Appearance (Form 2)</a>&nbsp;</li>
+                                        </ul>
+                                    </td>
+                                    <td>
+                                        Leave to Appeal <span style={{ color:'gray' }}>(with court's permission)</span>
+                                        <ul>
+                                            <li><a id="form2" href="/forms/form.2.html" target="_self">Notice of Appearance (Form 2)</a>&nbsp;</li>
+                                        </ul>
+                                    </td>
+                                </tr></table>
+                            </div>
+                        </td>
+                    </tr>
+                </table>                
+            </div>
+        )
+    }
+}
+export default SelectForm;
