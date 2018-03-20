8690fe12a1a4a14432db6958564da73715ecfb27
Eric Mignot <eric.mignot@gmail.com>
2018-03-20 15:39:58 -0700

    new mockup for active-forms component displayed in dashboard

--- src/forms/active.forms.js
+++ src/forms/active.forms.js
@@ -7,7 +7,7 @@ class ActiveForms extends Component {
         return (
             <div id="active-forms" className="form-section">
                 <div>
-                    <h3 style={{ display:'inline-block' }}>My Cases</h3>
+                    <h3 style={{ display:'inline-block' }}>Cases Reminders</h3>
                     <div style={{ display:'inline-block', float:'right', marginTop:'15px' }}>
                         <a href="/my-applications.html" className="btn btn-primary round-borders">View All Cases</a>
                     </div>
@@ -17,7 +17,6 @@ class ActiveForms extends Component {
                         <tr className="header">
                             <td></td>
                             <td>File #</td>
-                            <td>Form</td>
                             <td>Parties</td>
                             <td>Status                                
                             </td>
@@ -29,36 +28,32 @@ class ActiveForms extends Component {
                         <tr>
                             <td className="near-deadline"><i className="fas fa-exclamation-circle"></i></td>
                             <td>3456769</td>
-                            <td>Notice of Appearance (Form-2)</td>
                             <td>Moreno / McLane</td>
-                            <td>Draft</td>
+                            <td>draft</td>
                             <td style={{ color:'rgb(255, 0, 0)' }}>2018-03-20</td>
                             <td>2018-03-12</td>
                         </tr>
                         <tr>
                             <td></td>
                             <td>2098709</td>
-                            <td>Notice of Appearance (Form-2)</td>
                             <td>Moreno / McLane</td>
-                            <td>Draft</td>
+                            <td>rejected</td>
                             <td>2018-04-05</td>
                             <td>2018-03-01</td>
                         </tr>
                         <tr>
                             <td></td>
                             <td>2098710</td>
-                            <td>Notice of Appearance (Form-2)</td>
                             <td>Moreno / McLane</td>
-                            <td>Draft</td>
+                            <td>submitted</td>
                             <td>2018-04-06</td>
                             <td>2018-03-01</td>
                         </tr>
                         <tr>
                             <td></td>
                             <td>2098711</td>
-                            <td>Notice of Appearance (Form-2)</td>
                             <td>Moreno / McLane</td>
-                            <td>Draft</td>
+                            <td>withdrawn</td>
                             <td>2018-04-07</td>
                             <td>2018-03-01</td>
                         </tr>
