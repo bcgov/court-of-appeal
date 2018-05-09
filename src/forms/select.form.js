a704fe944e77d64030ed45dd74db7290e784a865
perkinss <perkins.susan@gmail.com>
2018-05-09 15:57:18 -0700

    - Update the form 2 lozenges - Make the appeal button open form 2 (for now)

--- src/forms/select.form.js
+++ src/forms/select.form.js
@@ -21,11 +21,10 @@ class SelectForm extends Component {
 
                 <div id="steps" className="small-steps">
                     <ol>
-                        <li className="small-steps"><span className="step-circle in-progress small-steps">1</span><span className="step-title in-progress small-steps">Select form</span></li>
                         <li className="small-steps"><span className="step-circle future small-steps">2</span><span className="step-title future small-steps">Complete form</span></li>
-                        <li className="small-steps"><span className="step-circle future small-steps">3</span><span className="step-title future small-steps">Pay</span></li>
-                        <li className="small-steps"><span className="step-circle future small-steps">4</span><span className="step-title future small-steps">Submit</span></li>
-                        <li className="small-steps"><span className="step-circle future small-steps">5</span><span className="step-title future small-steps">Notfications</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">3</span><span className="step-title future small-steps">Preview</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">3</span><span className="step-title future small-steps">Submit</span></li>
+                        <li className="small-steps"><span className="step-circle future small-steps">4</span><span className="step-title future small-steps">Pay</span></li>
                     </ol>
                 </div>
 
@@ -35,14 +34,14 @@ class SelectForm extends Component {
                             <tr>
                                 <td style={{ width:'50%', textAlign:'center'}}>
                                     <a href="/" className="btn btn-primary round-borders start-an-appeal">
-                                        Start an Appeal
-                                        <i className="fas fa-play"></i>
+                                        Start an Appeal &nbsp;
+                                        <i className="fa fa-play"/>
                                     </a>
                                 </td>
                                 <td style={{ width:'50%', textAlign:'center'}}>
-                                    <a href="/" className="btn btn-primary round-borders respond-to-appeal">
-                                        Respond to Appeal
-                                        <i className="fas fa-play"></i>
+                                    <a href="forms/form.2.html" className="btn btn-primary round-borders respond-to-appeal">
+                                        Respond to Appeal &nbsp;
+                                        <i className="fa fa-play"/>
                                     </a>
                                 </td>
                             </tr>
