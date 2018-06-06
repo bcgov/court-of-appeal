736125fef356c8f68fb3bcccb0b6ecd150e7e8db
perkinss <perkins.susan@gmail.com>
2018-06-06 16:41:59 -0700

    Improvements to journeymap: * Update the alt so it specifies exactly that there's an alt in the props, otherwise we get warnings in the console. * Update the render so it doesn't compute the maps if it's not yet being rendered.

--- src/components/JourneyMap.js
+++ src/components/JourneyMap.js
@@ -9,24 +9,28 @@ class JourneyMap extends React.Component {
     }
 
     render() {
-        let map = null;
-        let targetAreas = this.getAreas();
 
-        return (
+        if(this.props.mapProps) {
+            let targetAreas = this.getAreas();
+            return (
                 <div className="form-section ">
                     <h2>Your Appeal Process</h2>
                     <p>{this.props.subTitle}</p>
-                    <img  className="journey-map-image" src={this.props.mapSrc} useMap="#journeymap" alt="Appeal Journey Map" />
-                    <map name="journeymap">
+                    <img  className="journey-map-image" src={this.props.mapSrc} useMap="#journeymap" alt="Your Appeal Journey Map" />
+                    <map name="journeymap" >
                         {targetAreas}
                     </map>
                 </div>
             );
+        } else {
+            return null;
+        }
+
     }
 
     getAreas() {
         return this.props.mapProps.map((prop, key) => {
-            return <area key={key} shape="rect" {...prop}/>
+            return <area alt={prop.alt} key={key} shape="rect" {...prop}/>
         });
     }
 
