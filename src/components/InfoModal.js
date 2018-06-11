9ed0f428f29104f2c8f9ed41bfb8b486160c09ff
perkinss <perkins.susan@gmail.com>
2018-06-11 14:33:54 -0700

    Top level info modal section: icon, title, vertical bar.

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -4,7 +4,6 @@ class InfoModal extends Component {
 
     render() {
         let content = null;
-        console.log(this.props);
         if (this.props.show) {
             content =  <div id="info-modal" className="modal" style={{display: 'block'}} >
                 <div className="modal-title  not-printable">
@@ -12,24 +11,39 @@ class InfoModal extends Component {
                     The Factum and Appeal Book
                 </div>
                 <div className="modal-content info-modal">
-                    <h2><strong>30 days</strong></h2>
-                    <h2>Filing and service deadline after filing the Appeal Record</h2>
-
-                    <ol>
-                        <li>Complete either the .DOCs or .PDFs below:
-                            <ul>
-                                <li>Factum (Form 10) </li>
-                                <li> *Optional - Transcript and Extract Book (Form 13)</li>
-                                <li>Appeal Book (Form 12)</li>
-                            </ul>
-                        </li>
-                        <li>
-                            File the indicated number of copies to the registry.
-                        </li>
-                        <li>
-                            Serve one copy of each document to respondent.
-                        </li>
-                    </ol>
+                    <div className="row info-modal">
+                        <div className="col col-lg-1 col-md-1">
+                            <div shape="circle" className="info-modal-icon-container">
+                                <img src="icons/icon-clock.svg" className="info-modal-icon"/>
+                            </div>
+                        </div>
+                        <div className="col col-lg-11 col-md-11">
+                            <p className="deadline">30 days</p>
+                            <p>Filing and service deadline after filing the Appeal Record</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col col-lg-1 col-md-1">
+                            <div className="vertical-line" style={{height:'150px', marginTop:'-20px'}}/>
+                        </div>
+                        <div className="col col-lg-11 col-md-11">
+                            <ol>
+                                <li>Complete either the .DOCs or .PDFs below:
+                                    <ul>
+                                        <li>Factum (Form 10) </li>
+                                        <li> *Optional - Transcript and Extract Book (Form 13)</li>
+                                        <li>Appeal Book (Form 12)</li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    File the indicated number of copies to the registry.
+                                </li>
+                                <li>
+                                    Serve one copy of each document to respondent.
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
 
                     <h2><strong>7 days</strong></h2>
                     <h2>Filing and service deadline after receiving the Factum</h2>
