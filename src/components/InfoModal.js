cffdf3df36349f9718b1b2405a2b544dca48da9f
perkinss <perkins.susan@gmail.com>
2018-06-11 15:38:10 -0700

    Improve the factum modal styling * Add the other icons, * Add horizontal rules, * Add vertical lines * Improve spacing

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -14,20 +14,20 @@ class InfoModal extends Component {
                     <div className="row info-modal">
                         <div className="col col-lg-1 col-md-1">
                             <div shape="circle" className="info-modal-icon-container">
-                                <img src="icons/icon-clock.svg" className="info-modal-icon"/>
+                                <img src="icons/icon-clock.svg" className="info-modal-icon info-modal-clock"/>
                             </div>
                         </div>
                         <div className="col col-lg-11 col-md-11">
-                            <p className="deadline">30 days</p>
-                            <p>Filing and service deadline after filing the Appeal Record</p>
+                            <div className="deadline">30 days</div>
+                            <div style={{marginBottom:'20px'}}>Filing and service deadline after filing the Appeal Record</div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col col-lg-1 col-md-1">
-                            <div className="vertical-line" style={{height:'150px', marginTop:'-20px'}}/>
+                            <div className="vertical-line" style={{height:'186px', marginTop: '-20px'}}/>
                         </div>
                         <div className="col col-lg-11 col-md-11">
-                            <ol>
+                            <ol style={{fontWeight:"bolder"}}>
                                 <li>Complete either the .DOCs or .PDFs below:
                                     <ul>
                                         <li>Factum (Form 10) </li>
@@ -42,37 +42,77 @@ class InfoModal extends Component {
                                     Serve one copy of each document to respondent.
                                 </li>
                             </ol>
+                            <hr />
+                        </div>
+
+                    </div>
+
+                    <div className="row info-modal">
+                        <div className="col col-lg-1 col-md-1">
+                            <div shape="circle" className="info-modal-icon-container">
+                                <img src="icons/icon-share.svg" className="info-modal-icon"/>
+                            </div>
+                        </div>
+                        <div className="col col-lg-11 col-md-11">
+                            <div className="deadline">7 days</div>
+                            <div style={{marginBottom:'20px'}}>Filing and service deadline after receiving the Factum</div>
                         </div>
                     </div>
 
-                    <h2><strong>7 days</strong></h2>
-                    <h2>Filing and service deadline after receiving the Factum</h2>
+                    <div className="row">
+                        <div className="col col-lg-1 col-md-1">
+                            <div className="vertical-line" style={{height:'140px', marginTop: '-20px'}}/>
+                        </div>
+                        <div className="col col-lg-11 col-md-11">
+                            <ol style={{fontWeight:"bolder"}}>
+                                <li>
+                                    IF you would like to reply, please complete either the
+                                    .DOC or .PDF document below:
+                                    <ul>
+                                        <li> Reply (Form 11)</li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    File the indicated number of copies to the registry.
+                                </li>
+                                <li>
+                                    Serve one copy of each document to respondent.
+                                </li>
+                            </ol>
+                            <hr/>
+                        </div>
 
+                    </div>
 
-                    <ol>
-                        <li>
-                            IF you would like to reply, please complete either the
-                            .DOC or .PDF document below:
-                            <ul>
-                                <li> Reply (Form 11)</li>
-                            </ul>
-                        </li>
-                        <li>
-                            File the indicated number of copies to the registry.
-                        </li>
-                        <li>
-                            Serve one copy of each document to respondent.
-                        </li>
-                    </ol>
+                    <div className="row info-modal">
+                        <div className="col col-lg-1 col-md-1">
+                            <div shape="circle" className="info-modal-icon-container">
+                                <img src="icons/icon-info.svg" className="info-modal-icon"/>
+                            </div>
+                        </div>
+                        <div className="col col-lg-11 col-md-11">
+                            <p> You may also be served a copy of the respondent’s Transcript Extract Book.
+                                This document is for your awareness only and you do not have to respond
+                                to it.</p>
+                        </div>
+                    </div>
+                    <div className="row info-modal">
+                        <div  style={{fontWeight:"bolder",marginBottom:'20px'}} className="col col-lg-11 col-md-11 col-sm-11 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+                           Need help completing a document?
+                        </div>
+                    </div>
+                    <div className="row info-modal">
+                        <div className="col col-lg-3 col-md-3 col-lg-offset-1 divider">
+                            <a href=" https://www.courtofappealbc.ca/respondent-guidebook">Visit: SRL Guidebook</a>
+                        </div>
+                        <div className="col col-lg-4 col-md-4 divider right-info-cols">
+                            <a href="mailto:courts.cso@gov.bc.ca">Email: Courts.cso@gov.bc.ca</a>
+                        </div>
+                        <div className="col col-lg-4 col-md-4 right-info-cols">
+                            Call: 1-800-663-6102
+                        </div>
+                    </div>
 
-                    <p>
-                    You may also be served a copy of the respondent’s Transcript Extract Book.
-                    This document is for your awareness only and you do not have to respond
-                        to it.</p>
-
-
-                    <p>Need help completing a document?</p>
-                    Visit: SRL Guidebook    |   Email: Courts.cso@gov.bc.ca    |    Call: 1-800-663-6102
 
                 </div>
 
