b418ce29edfd4f1ab45aa210808d5e0b56013aeb
perkinss <perkins.susan@gmail.com>
2018-06-26 11:50:28 -0700

    initial version of appearance popup based on another popup

---
+++ src/components/infopopups/AppearancePopup.js
@@ -0,0 +1,88 @@
+import React, { Component }from 'react';
+import './infopopup.css';
+import Help from './Help';
+
+class AppearancePopup extends Component {
+
+    constructor(props) {
+        super(props);
+        let contentMap = [
+            {
+                line: "Complete either the .DOCs or .PDFs below:",
+                rows: [
+                    { description: "Factum (Form 10)", times: "4 x", link1: "DOC", link2: "PDF" },
+                    { description: "*Optional - Transcript and Extract Book (Form 13)", times: "1 x", link1: "DOC", link2: "PDF" },
+                    { description: "Appeal Book (Form 12)", times: "4 x", link1: "DOC", link2: "PDF" },
+                ]
+            },
+            "File the indicated number of copies to the registry.",
+            "Serve one copy of each document to respondent."
+        ];
+
+        let contentMap2 = [
+            {
+                line: "IF you would like to reply, please complete either the .DOC or .PDF document below:",
+                rows: [{description: "Reply (Form 11)", times: "4x", link1: "DOC", link2: "PDF"}]
+            },
+            "File the indicated number of copies to the registry.",
+            "Serve one copy of each document to respondent."
+        ];
+
+        this.sections = [{
+            expandable: true,
+            sectionHeading: "Have you filed an Appeal Record ?",
+            iconSrc: "icons/icon-share.svg",
+            iconClass: "info-modal-icon",
+            deadlinePhrase: {first: "File a Factum and Appeal Book ", last: " of filing an Appeal Record."},
+            deadline: " within 30 days ",
+            lineHeight: '252px',
+            last: false,
+            expanded: true,
+            contentMap: contentMap
+        }, {
+            expandable: true,
+            sectionHeading: "Were you served with a respondent's Factum?",
+            iconSrc :"icons/icon-share.svg",
+            iconClass: "info-modal-icon",
+            deadline:" within 7 days ",
+            deadlinePhrase: {first: "File and serve a Reply ", last: " of receiving a Respondent's Factum."},
+            lineHeight: '221px',
+            last: false,
+            contentMap: contentMap2
+        }, {
+            expandable: true,
+            sectionHeading: "Were you served with a respondent's Transcript Extract Book?",
+            iconSrc: "icons/icon-info.svg",
+            iconClass: "info-modal-icon",
+            deadline: null,
+            lineHeight: null,
+            VLProps: null,
+            last: true,
+            content: [" You may also be served a copy of the respondent’s Transcript Extract Book.  " +
+            " This document is for your awareness only and you do not have to respond to it."]
+        }]
+    }
+
+    render() {
+
+        let sections = this.props.getSections(this.sections);
+        return (
+            <div id="info-modal" className="modal" style={{display: 'block'}} >
+                <div className="info-modal-title ">
+                    <span id="close-modal" onClick={this.props.close}>&times;</span>
+                    The Factum and Appeal Book
+                </div>
+                <div className="info-modal-content">
+                    {sections}
+                    <Help
+                        URL="https://www.courtofappealbc.ca/appellant-guidebook"
+                        URLName="Visit: SRL Guidebook"
+                    />
+                </div>
+
+            </div>
+        );
+    }
+
+}
+export default AppearancePopup;
