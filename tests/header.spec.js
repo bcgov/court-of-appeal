730fcb48a88ed865557e91467c9f87fe919a2afc
Eric Mignot <eric.mignot@gmail.com>
2018-07-03 15:36:26 -0700

    document current behaviour of Header

---
+++ tests/header.spec.js
@@ -0,0 +1,24 @@
+require('./support/fake.dom');
+import React from 'react';
+import Header from '../src/Header';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+let DefaultService = require('../src/service/default.service');
+
+describe('Header', ()=>{
+
+    test('default service', ()=>{
+        let instance = mount(<Header/>).instance();
+        
+        expect(instance.service instanceof DefaultService).toEqual(true);  
+    });
+    test('displays name of connected user', ()=>{
+        let document = mount(<Header service={{
+            getPersonInfo: (callback)=> { callback({ login:'login', name:'this-name' }); }
+        }}/>);
+        document.update();
+
+        expect(document.find('#header').at(0).html()).toContain('login');
+    });
+});
