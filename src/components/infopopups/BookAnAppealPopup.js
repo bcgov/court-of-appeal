f31c13f5641b49440ae30f4f7357645f12f9c5e0
perkinss <perkins.susan@gmail.com>
2018-07-05 10:13:20 -0700

    Add BookAnAppeal popup, make use of new gavel icon

---
+++ src/components/infopopups/BookAnAppealPopup.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Help from "./common/Help";
+
+class BookAnAppealPopup extends Component {
+
+    constructor(props) {
+        super(props);
+
+        let content = [
+            {
+                URLName: "List of BC Court of Appeal locations and contacts",
+                URL: "http://www.courts.gov.bc.ca/court_of_appeal/court_locations_and_contacts.aspx",
+            }
+        ];
+
+        this.sections = [{
+            expandable: false,
+            expanded: true,
+            sectionHeading: "Contact your registry to reserve a date and time for your appeal. ",
+            iconSrc: "/icons/icon-hearing.svg",
+            iconClass: "info-modal-icon",
+            deadline: null,
+            deadlinePhrase: null,
+            lineHeight: null,
+            content: content
+        }]
+    }
+
+    render () {
+        return  (
+            <div id="info-modal" className="modal" style={{display: 'block'}} >
+                <div className="info-modal-title ">
+                    <span id="close-modal" onClick={this.props.close}>&times;</span>
+                    Book Appeal Date
+                </div>
+                <div className="info-modal-content">
+                    {this.props.getSections(this.sections)}
+                    <Help
+                        URL="https://www.courtofappealbc.ca/appellant-guidebook"
+                        URLName="Visit: SRL Guidebook"
+                    />
+                </div>
+            </div>
+        );
+    }
+
+} export default BookAnAppealPopup;
