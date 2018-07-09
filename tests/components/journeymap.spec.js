e0e7e8a1617942834aada0f6cafb0fe44afbd5fd
perkinss <perkins.susan@gmail.com>
2018-07-09 16:03:14 -0700

    improve coverage for journeymap

---
+++ tests/components/journeymap.spec.js
@@ -0,0 +1,63 @@
+require('../support/enzyme.setup');
+import React from 'react';
+import { mount } from 'enzyme';
+import JourneyMap from "../../src/components/JourneyMap";
+import InfoPopup from "../../src/components/infopopups/common/InfoPopup";
+
+describe.only('JourneyMap', ()=> {
+
+    describe('JourneyMap builds', ()=>{
+
+        test.only('returns null when there are no props', () => {
+            let journeymap = mount(<JourneyMap title="map title" subtitle="subtitle"/>);
+            let view = journeymap.instance().render();
+
+            expect(view).toEqual(null);
+        });
+
+        let journeymap;
+        let mapSrc = "/images/journeymap/journey-map_respondent-notice-of-appeal.png";
+        let mapProps = [{coords: "75,10,180,200", function: "appearance", alt: "info about filing a notice of appearance"}];
+
+        beforeEach(()=> {
+            journeymap = mount(
+                <JourneyMap
+                    title={"max wasn't here"}
+                    subTitle={"where's max?"}
+                    mapSrc={mapSrc}
+                    mapProps={mapProps}
+                />);
+        });
+
+        test.only('can build JourneyMap with a modal that can open and close', () => {
+
+            let instance = journeymap.instance();
+
+            expect(journeymap.find('#journey-map-image').prop('src')).toEqual(mapSrc);
+
+            // journeymap.openInfoModal();
+            expect(instance.state.showInfoModal).toEqual(false);
+
+            instance.openInfoModal();
+            expect(instance.state.showInfoModal).toEqual(true);
+
+            instance.closeInfoModal();
+            expect(instance.state.showInfoModal).toEqual(false);
+
+            instance.openInfoModal();
+            expect(instance.state.showInfoModal).toEqual(true);
+            // e.target.id.includes('info-modal')
+
+            instance.handleClickOffModal({target: {}});
+            expect(instance.state.showInfoModal).toEqual(true);
+
+            instance.handleClickOffModal({target: {id: 'info-modal'}});
+            expect(instance.state.showInfoModal).toEqual(false);
+
+        });
+
+
+        //[{coords: "75,10,180,200", function: "appearance", alt: "info about filing a notice of appearance"}];
+
+    });
+});// 57,61,62,64,65
