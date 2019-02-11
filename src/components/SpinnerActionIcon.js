7cc1db9f965f702be04379d7bf11e2d31ffcb7d2
GDS_VICTORIA\eric.mignot <eric.mignot@gmail.com>
2019-02-11 15:41:57 -0800

    SpinnerActionIcon is now an anchor with href=#

--- src/components/SpinnerActionIcon.js
+++ src/components/SpinnerActionIcon.js
@@ -6,14 +6,14 @@ class SpinnerActionIcon extends SpinnerButton {
     buttonOrSpinner() {
         if (this.state.downloading) {
             return (
-                <a id={this.props.id} title={ this.props.tooltip }>
+                <a href="#" id={this.props.id} title={ this.props.tooltip }>
                     <span className="action-icon"><i className="oi oi-ellipses"></i></span>
                 </a>
             )
         }
         else {
             return (
-                <a id={this.props.id} onClick={this.props.onClick} title={ this.props.tooltip }>
+                <a href="#" id={this.props.id} onClick={this.props.onClick} title={ this.props.tooltip }>
                     <span className="action-icon"><span className={ this.props.content }></span></span>
                 </a>
             )
