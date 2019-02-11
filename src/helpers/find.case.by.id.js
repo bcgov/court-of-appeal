7bfedca05bdabc98bbe09f901c39618a7beacbb7
GDS_VICTORIA\eric.mignot <eric.mignot@gmail.com>
2019-02-11 15:25:19 -0800

    acknoledge IE does not provide Array.find

--- src/helpers/find.case.by.id.js
+++ src/helpers/find.case.by.id.js
@@ -1,8 +1,15 @@
 let findCaseById = function(options, then) {
-    let found = options.cases.find((item) => item.id === options.id );
+    let found = undefined
+    for (let i=0; i<options.cases.length; i++) {
+        let candidate = options.cases[i]
+        if (candidate.id == options.id) {
+            found = candidate
+            break
+        }
+    }
     if (found) { 
         then(found);
-    } 
+    }
 };
 
 export default findCaseById;
