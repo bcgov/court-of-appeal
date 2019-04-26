de191b27e94604d1d3bd39996f5981ce5f9f803c
perkinss <perkins.susan@gmail.com>
2019-04-26 07:32:01 -0700

    First moving Journey map plus some other cleanup and snapshot updates

--- src/components/journeymap/RespondToAppealJourneyMap.js
+++ src/components/journeymap/RespondToAppealJourneyMap.js
@@ -3,6 +3,9 @@ import FormIcon from './journeyicons/FormIcon';
 import GavelIcon from './journeyicons/GavelIcon';
 import Trail from './Trail';
 import EndCircle from './journeyicons/EndCircle';
+import RightTurn from './RightTurn';
+import LeftTurn from './LeftTurn';
+import ReturnTrail from "./ReturnTrail";
 let JOURNEY_TYPE = require('../../helpers/constants')
 let cn = require('classnames');
 
@@ -34,15 +37,15 @@ class RespondToAppealJourneyMap extends React.Component {
     
     render() {
         return (
-            <div className={"journey-map-container"}>
+            <div className={"journey-map-container"} >
                 <div className={cn("journey-start-circle", {"completed-step": this.state.steps[0].status !== 'new'})} />
 
                 <div
                     style={{marginLeft: '50px',
                         borderTop: '9px solid rgb(159, 191, 226)',
-                        width: '60%',
+                        width: '65%',
                         position: 'absolute',
-                        top: '25%',
+                        top: '92px',
                         zIndex: '1'
                     }}/>
 
@@ -79,7 +82,7 @@ class RespondToAppealJourneyMap extends React.Component {
                           completed={this.stepCompleted.bind(this)}
                           ready={this.props.isStepReady(2,this.state.steps)}
                     />
-                
+
               <Trail
                   className={"journey-trail-l1-moveable"}
                   completed={this.state.steps[1].status === 'completed'}
@@ -96,8 +99,61 @@ class RespondToAppealJourneyMap extends React.Component {
                           completed={this.stepCompleted.bind(this)}
                           ready={this.props.isStepReady(3, this.state.steps)}
                 />
-                <div className={cn("journey-circle-right",{'circling': this.state.steps[2].status === 'completed'})} />
-                <div className={"square"} />
+               <ReturnTrail
+                   steps={this.state.steps}
+               />
+                 <div
+                     style={{marginLeft: '141px',
+                         borderTop: '9px solid rgb(159, 191, 226)',
+                         width: '65%',
+                         position: 'absolute',
+                         top: '374px',
+                         zIndex: '1',
+                         marginBottom: '81px'
+                     }}/>
+                <GavelIcon style={{top:'65%', left: '31%'}}
+                           stepTitle={"The Hearing"}
+                           action={this.iconClicked.bind(this, 'respondenthearing')}
+                           active={true}
+                           order={4}
+                           status={this.getStepStatus(4)}
+                           completed={this.stepCompleted.bind(this)}
+                           ready={this.props.isStepReady(4, this.state.steps)}
+                />
+
+                <Trail
+                    className={"journey-trail-l1-moveable"}
+                    completed={this.state.steps[3].status === 'completed'}
+                    width={'30%'}
+                    style={{position: 'absolute', marginLeft:'30%', top:'70.3%',width: '30%'}}
+                    level={2}
+                />
+                <FormIcon style={{top:'69%', left: '57%'}}
+                          twoPages={false}
+                          stepTitle={"Court Order"}
+                          stepTitleOptional={"(if required)"}
+                          action={this.iconClicked.bind(this, 'courtorder')}
+                          active={true}
+                          order={5}
+                          status={this.getStepStatus(5)}
+                          completed={this.stepCompleted.bind(this)}
+                          ready={this.props.isStepReady(5, this.state.steps)}
+                />
+                <Trail
+                    className={"journey-trail-l1-moveable"}
+                    completed={this.state.steps[4].status === 'completed'}
+                    width={'30%'}
+                    style={{position: 'absolute', width: '30%', left: '60%', top: '70.3%'}}
+                    level={2}
+                />
+                <EndCircle
+                    stepTitle={"Appeal Process Complete"}
+                    action={this.iconClicked.bind(this,'respondentcomplete')}
+                    active={true}
+                    completed={this.state.steps[4].status === 'completed'}
+                    style={{top: '70%', left: '80%'}}
+
+                />
             </div>
         );
     }
---
+++ src/components/journeymap/ReturnTrail.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import RightTurn from "./RightTurn";
+import LeftTurn from "./LeftTurn";
+import Trail from "./Trail";
+import './returntrail.css';
+
+class ReturnTrail extends Component {
+
+
+    render() {
+        return (
+            <div style={{position: 'relative', top: '84px', width:'100%', height: '169px', marginBottom: '80px'}}>
+                 <LeftTurn
+                     steps={this.props.steps}
+                     style={{
+                         position: 'absolute',
+                         top: '56px',
+                         left: '67px',
+                         zIndex: '10'
+                     }}
+                     circling={this.props.steps[2].status === 'completed'}
+                 />
+                <Trail
+                    style={{
+                        width: '65%',
+                        left: '21%',
+                        position: 'absolute',
+                        top: '13.5px',
+                        zIndex: '11'
+                    }}
+                    className={"journey-return-trail"}
+                    completed={this.props.steps[2].status === 'completed'}
+                    width={'75%'}
+                    circling={this.props.steps[2].status === 'completed'}
+                />
+                <Trail
+                    style={{
+                        width: '53%',
+                        left: '21%',
+                        position: 'absolute',
+                        top: '14px',
+                        zIndex: '10'
+                    }}
+                    className={"journey-return-trail"}
+                    completed={false}
+                    width={'75%'}
+                    circling={false}
+                />
+                <RightTurn
+                    steps={this.props.steps}
+                    style={{position: 'absolute',
+                        left: '73%',
+                        top: '-115px'}}
+                    circling={this.props.steps[2].status === 'completed'}
+                />
+
+            </div>
+        )
+    }
+}
+export default ReturnTrail;
