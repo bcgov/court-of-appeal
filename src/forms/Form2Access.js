c845de57b2917992be08d0ddbfd370be84688887
Eric Mignot <eric.mignot@gmail.com>
2019-05-17 09:34:06 -0700

    exploring form access

---
+++ src/forms/Form2Access.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import ProgressStatusBar from '../components/progress/ProgressStatusBar';
+import './Form2Access.css';
+import DefaultService from "../service/default.service";
+
+class Form2Access extends Component {
+
+    constructor(props) {
+        super(props);
+        this.service = props.service;
+        this.state = props.location && props.location.state ? props.location.state : { caseNumber: '12345 (fake)'};
+        this.next = this.next.bind(this)
+    }
+
+    componentDidMount() {
+        if (this.service == null) {
+            let window = this.element.ownerDocument.defaultView;
+            this.service = new DefaultService(window);
+        }
+    }
+
+    render() {
+        return (
+            <div id="topicTemplate" className="template container gov-container form" ref={ (element)=> {this.element = element }}>
+
+                <div id="breadcrumbContainer">
+                    <ol className="breadcrumb">
+                        <li>
+                            <a id="home" href={this.homePath}>Home</a>
+                        </li>
+                        <li>
+                            <a href="">Access</a>
+                        </li>
+                    </ol>
+                </div>
+
+                  <ProgressStatusBar
+                      activeStep={1}
+                      steps={["Access","Form 2","Preview","Payment"]}
+                  />
+
+                <div className="row">
+                    <div id="viewFormModal" className="col col-lg-12 col-md-12 col-sm-12 preview-form">
+                        <div className="preview-title  not-printable">
+                            Notice of Appearance Access
+                        </div>
+                        <div className="form-section">
+                            <div class="access-case-number-reminder">
+                                Court of Appeal Case #{ this.state.caseNumber }
+                            </div>
+                            The following users will have access to the package containing this Notice of Appearance once it is submitted.
+                            <div style={{textAlign:'right', paddingTop:'15px'}}>
+                                <button id="continue-to-form" onClick={this.next} className="btn btn-primary round-borders action-button">Fill Notice of Appeal form </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    next() {
+        this.props.history.push({pathname: process.env.PUBLIC_URL + '/',state: {}});
+    }
+}
+export default Form2Access;
