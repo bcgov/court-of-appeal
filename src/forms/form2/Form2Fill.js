7537840d873736495bc50aeb6a626cc08d63e787
Eric Mignot <eric.mignot@gmail.com>
2019-05-19 11:15:54 -0700

    Form2Fill now display respondent selection

--- src/forms/form2/Form2Fill.js
+++ src/forms/form2/Form2Fill.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import ReactTooltip from "react-tooltip";
 import ProgressStatusBar from '../../components/progress/ProgressStatusBar';
 import '../Form.css';
 import './Form2Fill.css';
 import DefaultService from "../../service/default.service";
+import RespondentListSelect from "../../components/fields/RespondentListSelect";
 
 class Form2Fill extends Component {
 
@@ -13,6 +15,9 @@ class Form2Fill extends Component {
         this.state = props.location && props.location.state ? props.location.state : {};
         console.log(this.state);
         this.backToAccess = this.backToAccess.bind(this)
+        this.selectedRespondents = this.selectedRespondents.bind(this)
+        this.updateSelectedRespondents = this.updateSelectedRespondents.bind(this)
+        this.selectAllRespondents = this.selectAllRespondents.bind(this);
     }
 
     componentDidMount() {
@@ -20,6 +25,7 @@ class Form2Fill extends Component {
             let window = this.element.ownerDocument.defaultView;
             this.service = new DefaultService(window);
         }
+        this.selectAllRespondents()
     }
 
     render() {
@@ -70,15 +76,79 @@ class Form2Fill extends Component {
                             <div className="section-title">
                                 Style of Proceeding (Parties) in Case
                             </div>
+
+                            <div className="row  proceeding-style">
+                                <div className="col-lg-1 col-md-1 col-sm-2 col-xs-2 proceeding-style-col">BETWEEN:</div>
+
+                                <div className="col-lg-9 col-md-9 col-sm-8 col-xs-8" id="appellant-name">{
+                                    this.state.parties.appellants.map( (item) => item.name ).join(', ')
+                                    }
+                                </div>
+                                <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2 proceeding-style-col">Appellant{this.state.parties.appellants.length > 1 ? 's' : '' }</div>
+                            </div>
+                            <div className="row  proceeding-style">
+                                <div className="col-lg-1 col-md-1 col-sm-2 col-xs-2 proceeding-style-col">AND:</div>
+
+                                    <div className="col-lg-9 col-md-9 col-sm-8 col-xs-8" id="respondent-name" >
+                                        {this.state.parties.respondents.map( (item) => item.name ).join(', ')}
+                                    </div>
+
+                                <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2 proceeding-style-col">Respondent{this.state.parties.respondents.length > 1 ? 's' : '' }</div>
+                            </div>
+
+                            <div className="row ">
+                               <div className="col-lg-2 col-md-2 col-sm-6 col-xs-6 respondent-name-label">
+                                   <div style={{fontWeight: "bolder"}}>
+                                       Responding: &nbsp;
+                                       <i className="oi oi-question-mark" aria-hidden="true"
+                                          data-tip={this.state.parties.respondents.length > 1 ?
+                                              'What are the names of the parties responding to the appeal?' :
+                                              'What is the name of the party responding to the appeal?'}/>
+                                  </div>
+                               </div>
+                           </div>
+
+                           <div className="row proceeding-style">
+                              <div className="col-lg-offset-1 col-md-offset-1 col-lg-9 col-md-9 col-sm-8 col-xs-8" id="respondent-select" >
+                                  <RespondentListSelect
+                                      selectedRespondents={this.selectedRespondents()}
+                                      respondents={this.state.parties.respondents}
+                                      handleFieldChange={this.updateSelectedRespondents}
+                                      id={"respondent-list"}
+                                      isMulti={true}
+                                      name={"form2.respondentList"}
+                                  />
+                              </div>
+                           </div>
                         </div>
                     </div>
                 </div>
+
+                <ReactTooltip/>
             </div>
         );
     }
 
     backToAccess() {
         this.props.history.push({pathname: process.env.PUBLIC_URL + '/access',state: { caseNumber:this.state.caseNumber, parties:this.state.parties }});
+    }
+    selectedRespondents() {
+       return this.state.parties.respondents.filter((respondent) => respondent.selected);
+    }
+    updateSelectedRespondents(event) {
+        let parties = this.state.parties
+        let respondents = parties.respondents
+        if ('clear' === event.action ) {
+            respondents.forEach((respondent) => respondent.selected = false)
+        } else if ('selectAll' === event.action) {
+            respondents.forEach((respondent) => respondent.selected = true);
+        } else {
+            respondents[event.target.value].selected = event.target.selected;
+        }
+        this.setState({ parties:parties })
+    }
+    selectAllRespondents() {
+        this.updateSelectedRespondents({ action:'selectAll' })
     }
 }
 export default Form2Fill;
