1ab3ef024314b216054f66a7e589761bddea0610
Wade Barnes <wade.barnes@shaw.ca>
2021-02-15 11:25:53 -0800

    Remove old OCP configurations and jenkins pipelines.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- api/openshift/create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "This script assumes:"
-echo "- you are running it in e-filing projet"
-echo "- a postgresql application already exists in the projects corresponding secret"
-
-oc process -f "./api-assuming-database-exists.json" "" | oc create -f -
