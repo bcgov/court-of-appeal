3bfa7c365eba27f3a5af9b38ea84297c96d9589e
marzmehr <marzmehr@uvic.ca>
2021-11-06 10:51:14 -0700

    CSOCA-192: Improve layout and styling of the process header, Add logout icon

--- web/src/components/process/Form2/components/Form2ProcessHeader.vue
+++ web/src/components/process/Form2/components/Form2ProcessHeader.vue
@@ -5,36 +5,38 @@
             <b-col cols="4" >
 
                 <div v-if="stepsCompleted.first">
-                    <b-icon-check-circle-fill class="ml-5 mt-3" variant="success" style="width: 57px;height: 57px;"/>
-                    <span class="text-success ml-3 mb-5" style="font-size: 18px; font-weight: 600px; display: inline-block;">Form 2</span>                
+                    <b-icon-check-circle-fill class="icon-complete ml-1 mt-2" variant="success" />
+                    <span class="step-complete text-success ml-4 mb-2">Form 2</span>                
                 </div>
                 <div v-else>
-                    <step-number class="ml-5 mt-3" v-bind:stepNumber="1" v-bind:active="true"/>
-                    <span class="text-dark" style="font-size: 18px; display: inline;">Form 2</span>
+                    <step-number class="ml-1 mt-2" v-bind:stepNumber="1" v-bind:active="true"/>
+                    <span class="text-dark step-incomplete">Form 2</span>
                 </div>               
 
             </b-col>
             <b-col cols="4" >
-                <div v-if="stepsCompleted.second">
-                    <b-icon-check-circle-fill class="ml-5 mt-3" variant="success" style="width: 57px;height: 57px;"/>
-                    <span class="text-success ml-3 mb-5" style="font-size: 18px; font-weight: 600px; display: inline;">Preview</span>                
+
+                <div v-if="stepsCompleted.second" style="text-align: center;">
+                    <b-icon-check-circle-fill class="icon-complete mt-2" variant="success" />
+                    <span class="step-complete text-success ml-4 mb-2" >Preview</span>                
                 </div>
-                <div v-else>
-                    <step-number class="ml-5 mt-3"  v-bind:stepNumber="2" v-bind:active="stepsCompleted.first"/>
-                    <span :class="stepsCompleted.first?'text-dark':'text-secondary'" style="font-size: 18px; display: inline;">Preview</span>
+                <div v-else style="text-align: center;">
+                    <step-number class="mt-2"  v-bind:stepNumber="2" v-bind:active="stepsCompleted.first"/>
+                    <span :class="stepsCompleted.first?'text-dark step-incomplete':'text-inactive step-incomplete'">Preview</span>
                 </div>               
                 
             </b-col>
             <b-col cols="4">
 
-                <div v-if="stepsCompleted.third">
-                    <b-icon-check-circle-fill class="ml-5 mt-3" variant="success" style="width: 57px;height: 57px;"/>
-                    <span class="text-success ml-2 mb-5" style="font-size: 18px; display: font-weight: 600px; inline-block;">Submit</span>                
+                <div v-if="stepsCompleted.third" style="float: right;">
+                    <b-icon-check-circle-fill class="icon-complete mt-2" variant="success"/>
+                    <span class="step-complete text-success mr-4 ml-3 mb-2">Submit</span>                
                 </div>
-                <div v-else>
-                    <step-number class="ml-5 mt-3"  v-bind:stepNumber="3" v-bind:active="stepsCompleted.first && stepsCompleted.second"/>
-                    <span :class="(stepsCompleted.first && stepsCompleted.second)?'text-dark':'text-secondary'" style="font-size: 18px; display: inline;">Submit</span>
-                </div>                 
+                <div v-else style="float: right;">
+                    <step-number class="mt-2"  v-bind:stepNumber="3" v-bind:active="stepsCompleted.first && stepsCompleted.second"/>
+                    <span :class="(stepsCompleted.first && stepsCompleted.second)?'text-dark step-incomplete mr-4':'text-inactive step-incomplete mr-4'">Submit</span>
+                </div>  
+                               
             </b-col>
         </b-row>        
         
@@ -65,6 +67,26 @@ export default class Form2ProcessHeader extends Vue {
     .step-number {
         border-radius: 50%;
         padding: 20px;
+    }
+
+    .step-complete {
+        font-size: 20px; 
+        font-weight: 700; 
+        display: inline-block; 
+        transform:translate(10px,-22px);
+    }
+
+    .step-incomplete {
+        font-size: 20px; 
+        font-weight: 700; 
+        display: inline-block; 
+        transform:translate(15px,10px);
+    }
+
+    .icon-complete {
+        width: 82px;
+        height: 82px;
+        transform:translate(15px,7px);
     }
 
 </style>
