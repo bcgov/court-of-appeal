ea6fc25c9f9bbd56d9a6c49c3ea0af4df4ec185c
marzmehr <marzmehr@uvic.ca>
2021-11-08 09:25:48 -0800

    CSOCA-192: Added backend functionality for form7Search

---
+++ api/api/form7_searching_utils/parse_form7.py
@@ -0,0 +1,125 @@
+import json
+
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+class Form7Parsing:
+
+    def extractCaseType(self, data):
+        if data.SearchByCaseNumberResponse:
+            return data.SearchByCaseNumberResult.CaseType
+        elif data.ViewCasePartyResult:
+            return data.ViewCasePartyResult.CaseType
+        else:
+            return None
+
+    def extractParties(self, data):
+        if data.ViewCasePartyResult and data.ViewCasePartyResult.Parties and data.ViewCasePartyResult.Parties.Party:
+            return data.ViewCasePartyResult.Parties.Party
+        else:
+            return None
+
+    def getName(self, name):       
+        if name:
+            return name
+        else:
+            return ""
+
+    def getFullName(self, person):        
+        return self.getName(person.FirstName) + ' ' + self.getName(person.LastName)
+
+    def getLawyer(self, party):
+        if party.LegalRepresentation:
+            return party.LegalRepresentation.Lawyer 
+        else:
+            return None
+
+    def getLawyerFirm(self, party):
+        lawyer = self.getLawyer(party)[0]
+        if lawyer:
+            return lawyer.Firm
+        else:
+            return None
+
+    def getLawyerFirmAddress(self, party):
+        firm = self.getLawyerFirm(party)
+        if firm is not None:
+            return {
+                "firmName": firm.FirmName,
+                "firmPhone": firm.Phone,
+                "addressLine1":firm.Address1,
+                "addressLine2":firm.Address2,
+                "city":firm.City,
+                "postalCode":firm.PostalCode,
+                "province":firm.Province
+            }
+        else:
+            return {"firmName": "","firmPhone": "","addressLine1":"","addressLine2":"","city":"","postalCode":"","province":""}
+
+    def buildPartyInfo(self, party):
+        info = {}
+        
+        if party.FirstName: 
+            info["name"] = self.getFullName(party)
+            info["firstName"] = party.FirstName
+            info["lastName"] = party.LastName
+        elif party.Organization:            
+            info["name"] = party.Organization
+            info["organization"] = party.Organization    
+        else:
+            raise Exception("Couldn't find person or organization in party.")
+
+        lawyer = self.getLawyer(party)
+
+        if lawyer is not None:
+            solicitor = {
+                "name": self.getFullName(lawyer[0]),
+                "counselFirstName": lawyer[0].FirstName,
+                "counselLastName": lawyer[0].LastName,
+            }            
+            solicitor.update(self.getLawyerFirmAddress(party))
+            info["solicitor"] = solicitor
+    
+        info["partyId"] = party.PartyID
+        return info
+
+    def getAppellants(self, parties):
+        appellants = list()
+        for party in parties:
+            if party.PartyRole == 'Appellant':
+                appellant = self.buildPartyInfo(party)
+                appellants.append(appellant)
+        
+        return appellants
+
+    def getRespondents(self, parties):        
+        respondents = list()
+        for party in parties:
+            if party.PartyRole == 'Respondent':
+                respondant = self.buildPartyInfo(party)
+                respondents.append(respondant)
+
+        return respondents
+
+    def parse(self, result):
+        
+        data = Struct(**result) 
+
+        parties = self.extractParties(data)
+        if parties is not None:       
+            appellants = self.getAppellants(parties)
+            respondents = self.getRespondents(parties)
+            print("______________")
+            print(appellants)
+            print("______________")
+            print("______________")
+            print(respondents)
+            return {
+                "parties":{
+                    "appellants": self.getAppellants(parties),
+                    "respondents": self.getRespondents(parties)
+                }
+            }
+        else:
+            return None
--- api/api/views/form7_search_view.py
+++ api/api/views/form7_search_view.py
@@ -1,97 +1,36 @@
-import os
 import logging
-from zeep import Client
+
 from zeep import helpers
-from zeep.cache import InMemoryCache
-from zeep.transports import Transport
-from requests import Session
-from requests.auth import HTTPBasicAuth
 
 from django.http import (
-    HttpResponseBadRequest,
-    HttpResponseNotFound, HttpResponseForbidden
+    HttpResponseNotFound
 )
 
 from rest_framework import permissions
 from rest_framework.response import Response
-from rest_framework.request import Request
 from rest_framework.views import APIView
 
-logger = logging.getLogger(__name__)
+from api.form7_searching_utils import Form7Search, Form7Parsing
 
-COA_USERNAME = os.environ.get("COA_USERNAME")
-COA_PASSWORD = os.environ.get("COA_PASSWORD")
-COA_SEARCH_ENDPOINT = os.environ.get("COA_SEARCH_ENDPOINT")
+logger = logging.getLogger(__name__)
 
 class Form7SearchView(APIView):
 
     permission_classes = (permissions.IsAuthenticated,)
 
+    def __init__(self):
+        self.form7_search = Form7Search()
+        self.parsing_form7 = Form7Parsing()
+   
     def get(self, request):
         caseNumber = request.query_params.get("caseNumber")
-        result = self.execute_search(caseNumber)
+         
+        result = self.form7_search.execute_search(caseNumber)
         if (result == 'NOT FOUND'):
             return HttpResponseNotFound()
 
-        return Response(helpers.serialize_object(result))
-
-    def __init__(self):
-        session = Session()
-        session.auth = HTTPBasicAuth(COA_USERNAME, COA_PASSWORD)
-        self.client = Client(
-            COA_SEARCH_ENDPOINT,
-            transport=Transport(cache=InMemoryCache(), session=session),
-        )
-
-    def _has_publication_ban(self, case_basics):
-        return (
-            "SecurityFlags" in case_basics
-            and case_basics.SecurityFlags is not None
-            and case_basics.SecurityFlags.SecurityFlag
-            and any(
-                x.Name == "Publication Ban"
-                for x in case_basics.SecurityFlags.SecurityFlag
-            )
-        )
-
-    def _has_restricted_files(self, case_basics):
-        return (
-            "SecurityFlags" in case_basics
-            and case_basics.SecurityFlags is not None
-            and case_basics.SecurityFlags.SecurityFlag
-            and any(
-                x.Name == "Family & Restricted Files"
-                for x in case_basics.SecurityFlags.SecurityFlag
-            )
-        )
-
-    def _has_family_law(self, case_basics):
-        return case_basics.HighLevelCategory == "Family Law"
-
-    def execute_search(self, case_number) -> {}:       
-        search_by_case_number = self.client.service.SearchByCaseNumber(case_number)
-        if search_by_case_number is None or search_by_case_number["CaseId"] == -1:
-            return self.handle_not_found(case_number)
-        elif search_by_case_number["CaseType"] == "Civil":
-            return self.handle_civil_search(search_by_case_number["CaseId"])
-        elif search_by_case_number["CaseType"] == "Criminal":
-            return {"SearchByCaseNumberResult": search_by_case_number}
-
-    def handle_not_found(self, case_number):
-        logger.info("Case not found: " + case_number)
-        return "NOT FOUND"
-
-    def handle_civil_search(self, case_id) -> {}:
-        case_basics = self.client.service.ViewCaseBasics(case_id)
-        if self._has_publication_ban(case_basics):
-            logger.info("Publication ban found.")
-            return {"ViewCaseBasicsResult": case_basics}
-        elif self._has_restricted_files(case_basics):
-            logger.info("Restricted files found.")
-            return {"ViewCaseBasicsResult": case_basics}
-        elif self._has_family_law(case_basics):
-            logger.info("Family Law found.")
-            return {"ViewCaseBasicsResult": case_basics}
-
-        case_party = self.client.service.ViewCaseParty(case_id)
-        return {"ViewCasePartyResult": case_party, "ViewCaseBasicsResult": case_basics}
+        parseForm7 = self.parsing_form7.parse(result)
+        if parseForm7 is not None:
+            return Response(helpers.serialize_object(parseForm7))
+        else:
+            return HttpResponseNotFound()            
