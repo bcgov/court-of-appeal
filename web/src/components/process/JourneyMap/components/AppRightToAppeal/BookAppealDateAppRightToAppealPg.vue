cd1943e41a9a0b9b9e4d4acd2d37d4e7d8e3a5cd
marzmehr <marzmehr@uvic.ca>
2022-05-16 09:12:48 -0700

    CSOCA-340: Add initial changes to the Applying-pathway in the journey maps

--- web/src/components/process/JourneyMap/components/AppRightToAppeal/BookAppealDateAppRightToAppealPg.vue
+++ web/src/components/process/JourneyMap/components/AppRightToAppeal/BookAppealDateAppRightToAppealPg.vue
@@ -2,17 +2,75 @@
     <b-card class="bg-white border-white w-90">
 
         <b-row class="mt-3">            
-            <div style="text-align: left; font-weight: bold;">
-                Contact your registry to reserve a date and time for your appeal.
-            </div>            
-        </b-row>        
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                When is an appeal ready for hearing?
+            </div>     
+            <p>
+                An appeal is ready for hearing on the date when 
+                the appellant has filed both:
+                <ul>
+                    <li>the appellant’s factum and</li>
+                    <li>
+                        a copy of each order being appealed, in 
+                        the form that the order was entered in 
+                        the court appealed from, or
+                    </li>
+                    <li>on the date specified by a justice or registrar</li>
+                </ul>                
+            </p>       
+        </b-row>  
 
-        <b-row class="mt-4 ml-3" >            
+        <b-row class="mt-0">            
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                How do I obtain an appeal hearing date?
+            </div>     
+            <p>
+                After an appeal is ready for hearing, you must, without delay,
+                <ul>
+                    <li>obtain a hearing date for the appeal by 
+                        contacting the court registry (hyperlink)
+                    </li>
+                    <li>
+                        inform the registrar is the parties disagree 
+                        on the length of time required for the hearing of the appeal.
+                    </li>                    
+                </ul>
+                Once you have obtained a hearing date, you must, 
+                without delay, file and serve a notice of hearing 
+                that has attached a copy of each order being appealed.                
+            </p>       
+        </b-row> 
+
+        <b-row class="mt-2">            
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                Can I expedite the hearing of the appeal?
+            </div>     
+            <p>
+                You may request to expedite an appeal by filing and 
+                serving a written request which must state the following:
+                <ul>
+                    <li>the nature of the appeal</li>
+                    <li>the reason for the request</li>
+                    <li>whether the other parties consent to expediting the appeal</li>
+                    <li>
+                        the proposed terms for expediting the hearing of the appeal including
+                        <ul>
+                            <li>schedule for the steps required to bring the appeal to hearing</li>
+                            <li>estimate of the time required for the hearing of the appeal</li>
+                        </ul>
+                    </li>
+                    <li>a list of proposed dates for the hearing of the appeal</li>                    
+                </ul>
+                                
+            </p>       
+        </b-row>          
+
+        <!-- <b-row class="mt-4 ml-3" >            
             <a 
                 href="http://www.courts.gov.bc.ca/court_of_appeal/court_locations_and_contacts.aspx"
                 target="_blank">BC Court of Appeal locations and contacts
             </a>                               
-        </b-row>        
+        </b-row>         -->
         
     </b-card>
 </template>
--- web/src/components/process/JourneyMap/components/AppRightToAppeal/TheHearingAppRightToAppealPg.vue
+++ web/src/components/process/JourneyMap/components/AppRightToAppeal/TheHearingAppRightToAppealPg.vue
@@ -1,30 +1,267 @@
 <template>
     <b-card class="bg-white border-white w-90">
 
-        <b-row class="mt-3">            
+        <b-row class="mt-3 h3">            
             <div style="text-align: left; font-weight: bold;">
                 For more information about the hearing process, click the topics below:
             </div>            
         </b-row>        
 
-        <b-row class="mt-4 ml-3" >            
-            <a 
-                href="https://www.courtofappealbc.ca/appellant-guidebook/3.3-how-to-prepare-for-the-hearing?ct=t(step-index-link)"
-                target="_blank">How do I prepare for my hearing?
-            </a>                               
+        <b-row class="mt-3">            
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                How do I prepare for my hearing?
+            </div>     
+            <p>
+                You may wish to review information about preparing for 
+                your appeal by clicking on the link below.
+            </p>       
+
+            <p class="mt-1 ml-3" >            
+                <a 
+                    href="https://www.courtofappealbc.ca/appellant-guidebook/3.3-how-to-prepare-for-the-hearing?ct=t(step-index-link)"
+                    target="_blank">How do I prepare for my hearing?
+                </a>                               
+            </p>
+        </b-row> 
+
+        <b-row class="mt-4" >
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                Points of law and authorities not cited in factum
+            </div> 
+            <p>
+                The court may permit a party to use arguments, raise 
+                points of law or cite authorities that were not used, 
+                raised or cited in a party’s factum.
+            </p>
+            <p>
+                You may submit a condensed book of authorities or a 
+                condensed book of evidence or both to the court at 
+                the hearing of an appeal provided a copy is able 
+                to be provided to each justice and each other party 
+                at the hearing of the appeal.
+            </p>            
+            <p>
+                Prepare a condensed book of authorities and/or a 
+                condensed book of evidence in accordance with the 
+                Completion Instructions.
+            </p>
+            <ul class="w-100">
+                <li>
+                    <a 
+                        href="https://www.courtofappealbc.ca/appellant-guidebook/2.8-replies?ct=t(sidebar-link)"
+                        target="_blank">Condensed Book of Authorities
+                    </a>
+                    <ul>                        
+                        <li>
+                            <b-row class="my-1 w-90">
+                                <b-col cols="8">
+                                    <a 
+                                        href=""
+                                        target="_blank">Paper Completion Instructions
+                                    </a>                                                                  
+                                </b-col>
+                                <b-col cols="3"></b-col>
+                                                            
+                                <b-col cols="1">
+                                    <b-button
+                                        href="https://www.bccourts.ca/Court_of_Appeal/practice_and_procedure/Forms/Form%2013.pdf"
+                                        target="_blank"
+                                        class="form-download-button p-1 bg-white text-primary border-primary">PDF
+                                    </b-button>                                    
+                                </b-col>
+                            </b-row>
+                        </li>
+                        <li>
+                            <b-row class="my-1 w-90">
+                                <b-col cols="8">
+                                    <a 
+                                        href="https://www.courtofappealbc.ca/appellant-guidebook/2.11-prepare-and-file-a-certificate-of-readiness?ct=t(sidebar-link)"
+                                        target="_blank">E-Completion Instructions
+                                    </a>                                                                     
+                                </b-col>
+                                <b-col cols="3"></b-col>
+                                
+                                <b-col cols="1">
+                                    <b-button
+                                        href="https://www.bccourts.ca/Court_of_Appeal/practice_and_procedure/Forms/fillable_forms/civil_rules_forms/Form14.pdf"
+                                        target="_blank"
+                                        class="form-download-button p-1 bg-white text-primary border-primary">PDF
+                                    </b-button>                                    
+                                </b-col>
+                            </b-row>
+                        </li>
+                    </ul>
+                </li>                
+                 <li>
+                    <a 
+                        href="https://www.courtofappealbc.ca/appellant-guidebook/2.8-replies?ct=t(sidebar-link)"
+                        target="_blank">Condensed Book of Evidence
+                    </a>
+                    <ul>                        
+                        <li>
+                            <b-row class="my-1 w-90">
+                                <b-col cols="8">
+                                    <a 
+                                        href=""
+                                        target="_blank">Paper Completion Instructions
+                                    </a>        
+                                </b-col>
+                                <b-col cols="3"></b-col>                                
+                                <b-col cols="1">
+                                    <b-button
+                                        href="https://www.bccourts.ca/Court_of_Appeal/practice_and_procedure/Forms/Form%2013.pdf"
+                                        target="_blank"
+                                        class="form-download-button p-1 bg-white text-primary border-primary">PDF
+                                    </b-button>                                    
+                                </b-col>
+                            </b-row>
+                        </li>
+                        <li>
+                            <b-row class="my-1 w-90">
+                                <b-col cols="8">
+                                    <a 
+                                        href="https://www.courtofappealbc.ca/appellant-guidebook/2.11-prepare-and-file-a-certificate-of-readiness?ct=t(sidebar-link)"
+                                        target="_blank">E-Completion Instructions
+                                    </a>                                                                     
+                                </b-col>
+                                <b-col cols="3"></b-col>
+                                
+                                <b-col cols="1">
+                                    <b-button
+                                        href="https://www.bccourts.ca/Court_of_Appeal/practice_and_procedure/Forms/fillable_forms/civil_rules_forms/Form14.pdf"
+                                        target="_blank"
+                                        class="form-download-button p-1 bg-white text-primary border-primary">PDF
+                                    </b-button>                                    
+                                </b-col>
+                            </b-row>
+                        </li>
+                    </ul>
+                </li> 
+            </ul>
         </b-row>
-        <b-row class="mt-2 ml-3" >            
-            <a 
-                href="https://www.courtofappealbc.ca/appellant-guidebook/3.4-what-happens-at-the-hearing?ct=t(sidebar-link)"
-                target="_blank">What will happen at my hearing?
-            </a>                               
+
+        <b-row class="mt-3">            
+                 
+            <p>
+                You may wish to review information about 
+                your hearing and what will happen after your hearing by clicking on the links below.
+            </p>       
+
+            <ul class="mt-1 ml-3" >     
+                <li>       
+                    <a 
+                        href="https://www.courtofappealbc.ca/appellant-guidebook/3.4-what-happens-at-the-hearing?ct=t(sidebar-link)"
+                        target="_blank">What will happen at my hearing?
+                    </a>                               
+                </li>
+                <li class="mt-1" >            
+                    <a 
+                        href="https://www.courtofappealbc.ca/appellant-guidebook/3.1-making-chambers-applications?ct=t(step-index-link)"
+                        target="_blank">What can I expect after my hearing?
+                    </a>   
+                </li>    
+                <li class="mt-1" >            
+                    <a 
+                        href="https://www.courtofappealbc.ca/appellant-guidebook/3.1-making-chambers-applications?ct=t(step-index-link)"
+                        target="_blank">Getting Judgment
+                    </a>   
+                </li>                         
+            </ul>
         </b-row>
-        <b-row class="mt-2 ml-3" >            
-            <a 
-                href="https://www.courtofappealbc.ca/appellant-guidebook/3.1-making-chambers-applications?ct=t(step-index-link)"
-                target="_blank">What can I expect after my hearing?
-            </a>                               
-        </b-row>        
+
+        <b-row class="mt-3">            
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                Court Order
+            </div>  
+            <p class="mt-2" style="display: inline-block;">
+                After judgment on an appeal has been given, the order of the court must be drawn 
+                up and entered in accordance with Rule 66 of the Court of Appeal Rules.
+            </p>       
+
+            <p class="mt-1 ml-3" >            
+                <a 
+                    href="https://www.courtofappealbc.ca/appellant-guidebook/3.1-making-chambers-applications?ct=t(step-index-link)"
+                    target="_blank">Orders
+                </a>                              
+            </p>
+        </b-row>
+
+        <b-row class="mt-3">            
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                Costs
+            </div>  
+            <p class="mt-2" style="display: inline-block;">
+                If you were awarded costs, you may apply to the registrar for an 
+                assessment of costs by bringing an application 
+                as outlined in Rule 53 and Rule 54 of the Court of Appeal Rules.
+            </p>       
+
+            <p class="mt-1 ml-3" >            
+                <a 
+                    href="https://www.courtofappealbc.ca/appellant-guidebook/3.1-making-chambers-applications?ct=t(step-index-link)"
+                    target="_blank">Application a bill of costs (Form 21)
+                </a>                              
+            </p>
+        </b-row>
+
+        <b-row class="mt-3" >
+            <div class="mb-2" style="text-align: left; font-weight: bold;">
+                Directions
+            </div>  
+            <p class="mt-2" style="display: inline-block;">
+                After a judgment on an appeal has been given, a party may apply to the court for directions 
+                under Section 45 of the Court of Appeal Act about costs if the parties disagree.
+            </p> 
+            <ol>
+                <li>
+                    Submit a written request to the registrar that includes the following:
+                    <ul>
+                        <li>Summary of the disagreement and the party’s position on the disagreement
+                            <!-- <b-row class="my-1 w-110">
+                                <b-col cols="8">Summary of the disagreement and the party’s position on the disagreement
+                                    <a 
+                                        href="https://www.courtofappealbc.ca/appellant-guidebook/2.8-replies?ct=t(sidebar-link)"
+                                        target="_blank">Summary of the disagreement and the party’s position on the disagreement
+                                    </a>                                    
+                                </b-col>                                
+                                <b-col cols="3" ></b-col>
+                                <b-col cols="1">
+                                    <b-button
+                                        href=""
+                                        target="_blank"
+                                        class="form-download-button p-1 bg-white text-primary border-primary">PDF
+                                    </b-button>                                    
+                                </b-col>
+                            </b-row> -->
+                        </li>
+                        <li>A proposed schedule for submissions by the parties or request to the registrar 
+                            to arrange a timetable for submissions
+                            <!-- <b-row class="my-1 w-110">
+                                <b-col cols="8">
+                                    <a 
+                                        href=""
+                                        target="_blank">Paper Completion Instructions
+                                    </a>
+                                                            
+                                </b-col>
+                                <b-col cols="3"></b-col>                                
+                                <b-col cols="1">
+                                    <b-button
+                                        href="https://www.bccourts.ca/Court_of_Appeal/practice_and_procedure/Forms/Form%2013.pdf"
+                                        target="_blank"
+                                        class="form-download-button p-1 bg-white text-primary border-primary">PDF
+                                    </b-button>                                    
+                                </b-col>
+                            </b-row> -->
+                        </li>
+                        
+                    </ul>
+                </li>
+                <li>
+                    Serve a copy of the written request on each party.
+                </li>
+            </ol>
+        </b-row>  
         
     </b-card>
 </template>
