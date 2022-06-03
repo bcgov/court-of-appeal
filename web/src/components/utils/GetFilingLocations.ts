0e0cb16d82d4e774cd1287c5c1a95f2a2874c9ae
marzmehr <marzmehr@uvic.ca>
2022-06-03 09:25:44 -0700

    minor fix

--- web/src/components/utils/GetFilingLocations.ts
+++ web/src/components/utils/GetFilingLocations.ts
@@ -1,20 +1,42 @@
 import store from '@/store/index';
-import Axios from "axios";
 import * as _ from 'underscore';
+import Vue from 'vue'
 
-export async function GetFilingLocations() {
+// export async function GetFilingLocations() {
 
-    var response = await Axios.get('/locations/?supreme=true');
+//     var response = await Axios.get('/locations/?supreme=true');
     
-    if(response?.data){     
+//     if(response?.data){     
 
-        const locationsInfo = response.data;
+//         const locationsInfo = response.data;
 
-        const sortedLocationsInfo = _.sortBy(locationsInfo, 'name');
+//         const sortedLocationsInfo = _.sortBy(locationsInfo, 'name');
 
-        store.dispatch('Common/UpdateLocationsInfo',sortedLocationsInfo); 
-        return true;
-    }
-    else 
-        return false;    
+//         store.dispatch('Common/UpdateLocationsInfo',sortedLocationsInfo); 
+//         return true;
+//     }
+//     else 
+//         return false;    
+// }
+
+export function GetFilingLocations() {
+
+    Vue.prototype.$http.get('/locations/?supreme=true')
+    .then(response => {
+        if(response?.data){     
+
+            const locationsInfo = response.data;
+
+            const sortedLocationsInfo = _.sortBy(locationsInfo, 'name');
+
+            store.dispatch('Common/UpdateLocationsInfo',sortedLocationsInfo); 
+            return true;
+        }
+        else 
+            return false;
+
+    },err => {
+        console.error(err);
+        return false;
+    });
 }
