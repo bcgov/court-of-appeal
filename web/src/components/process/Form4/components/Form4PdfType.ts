038f7e55774e45a82a18e6744a5a925482613ea0
marzmehr <marzmehr@uvic.ca>
2022-07-10 08:37:03 -0700

    minor edits

--- web/src/components/process/Form4/components/Form4PdfType.ts
+++ web/src/components/process/Form4/components/Form4PdfType.ts
@@ -3,13 +3,10 @@ import { form4DataInfoType } from "@/types/Information/Form4";
 export function GetForm4PdfType(result: form4DataInfoType){
     if(result.orderList.includes("leave to appeal"))
         return "MCH"
-    else if(result.orderList.includes(""))
+    else 
         return "MCT"
-    else {
-        if (result.orderList.includes("") ){
+    
 
-        }
-
-    }
+    
         
 }
