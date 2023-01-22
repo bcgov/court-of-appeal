bb0eb1f727725c5b246306401dd70fbbb8a91828
marzmehr <marzmehr@uvic.ca>
2023-01-22 10:43:56 -0800

    CSOCA-468: Change 'journey' wording in the journey maps

--- web/src/components/process/AppealProcess/pathwayInstructions/rspToLeaveInstructions.vue
+++ web/src/components/process/AppealProcess/pathwayInstructions/rspToLeaveInstructions.vue
@@ -6,7 +6,7 @@
             you were in is being appealed, and the appellant is applying to the court for leave to appeal. 
             If you would like to be updated about the status of the appeal, and would like to participate in 
             the appeal hearing, follow the instructions until the appeal process is complete. Start by clicking 
-            on the first form. As you progress through your appeal journey, check the box next to each step below 
+            on the first form. As you progress through your appeal, check the box next to each step below 
             to mark it as complete.
         </p>
         
