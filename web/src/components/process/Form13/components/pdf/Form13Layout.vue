1dfce1de40799471ad4af56fd31c8a43fbb77455
Taylor, Jonathan <jonathan.taylor@quartech.com>
2023-12-05 09:25:56 -0800

    CSOCA-507: Update form as requested

--- web/src/components/process/Form13/components/pdf/Form13Layout.vue
+++ web/src/components/process/Form13/components/pdf/Form13Layout.vue
@@ -1,104 +1,112 @@
 <template>
     <div>
 
-        <div>
-            <div style="color:#FFF; font-size:1px; width:0.1rem; height:0.1rem; margin:0; padding:0;"><b>i</b></div>
-        </div> 
-
         <div class="my-0">
-            <div class="text-right" >Court of Appeal File No. <b class="ml-3" style="text-transform: uppercase">{{result.formSevenNumber}}</b></div>       
+            <div class="text-right">FORM 13 <i>(RULE 67(2)(a) )</i></div>
         </div>
 
-<!-- <BETWEEN> -->
+        <div class="my-0">
+            <div class="text-center"><b>COURT OF APPEAL FOR BRITISH COLUMBIA</b></div>
+        </div>
+
+        <div>
+            <div style="color:#FFF; font-size:1px; width:0.1rem; height:0.1rem; margin:0; padding:0;"><b>i</b></div>
+        </div>
+
+        <div class="my-0">
+            <div class="text-right">Court of Appeal File No. <b class="ml-3" style="text-transform: uppercase">{{result.formSevenNumber}}</b></div>
+        </div>
+
+        <!-- <BETWEEN> -->
         <div class="mb-3 mx-0 row" style="font-weight: 700;">
             <div style="width:11%;">
-                BETWEEN: 
+                BETWEEN:
             </div>
         </div>
         <div class="my-3 mx-0 row" style="font-weight: 700;">
-            <div style="width:11%;"/>
+            <div style="width:11%;" />
             <div style="width:78%;">
                 <div style="font-weight: 200;" class="text-center mx-3">{{applicantNamesFull}}</div>
             </div>
-            <div style="width:11%;" class="text-center">Appellant<span v-if="applicantNames.length>1" >s</span></div>
+            <div style="width:11%;" class="text-center">Appellant(s)</div>
         </div>
 
-<!-- <AND> -->
+        <!-- <AND> -->
         <div class="my-3 mx-0 row" style="font-weight: 600;">
             <div style="width:11%;">
-                AND: 
+                AND:
             </div>
         </div>
         <div class="mt-3 mx-0 row" style="font-weight: 600;">
-            <div style="width:11%;" />                
+            <div style="width:11%;" />
             <div style="width:76%;">
                 <div style="font-weight: 200;" class="text-center mx-3">{{respondentNamesFull}}</div>
             </div>
-            <div style="width:13%;" class="text-center"> Respondent<span v-if="respondentNames.length>1" >s</span></div>
-        </div>        
+            <div style="width:13%;" class="text-center"> Respondent(s)</div>
+        </div>
 
-<!-- <ORDER-DATE> -->
+        <!-- <ORDER-DATE> -->
         <div class="mt-4 mb-4 mx-0 row">
             <div>
-                {{result.orderDate | beautify-date-full}} 
+                {{result.orderDate | beautify-date-full}}
             </div>
         </div>
 
-<!-- <WHEREAS:> -->
+        <!-- <WHEREAS:> -->
         <div class="mb-2 mt-2 mx-0 row" style="font-weight: 600;">
             <div>
-                WHEREAS: 
+                WHEREAS:
             </div>
         </div>
-        
-<!-- <Description> -->
+
+        <!-- <Description> -->
         <div class="my-2 ml-5 mr-0">
             <div class="mb-2 row">
                 (a) all parties have consented to this order,
             </div>
-            <div class="my-2 row">    
-                (b) no person involved is under any legal disability, and 
+            <div class="my-2 row">
+                (b) no person involved is under any legal disability, and
             </div>
-            <div class="my-2 row">    
-                (c) all parties have agreed to comply hereafter with the 
+            <div class="my-2 row">
+                (c) all parties have agreed to comply hereafter with the
                 time limits set forth in the Court of Appeal Act and Court of
-                Appeal Rules, 
+                Appeal Rules,
             </div>
         </div>
 
-<!-- <ORDERED> -->
+        <!-- <ORDERED> -->
         <div class="mt-4 mb-2 mx-0 row">
             <div>
-                <b>IT IS ORDERED</b> that the time set for {{applyingParties}} 
-                 to file and serve the {{applicationType}} is extended until
-                {{extensionDate | beautify-date-full-no-weekday}} 
+                <b>IT IS ORDERED</b> that the time set for {{applyingParties}}
+                to file and serve the {{applicationType}} is extended until
+                {{extensionDate | beautify-date-full-no-weekday}}
             </div>
         </div>
 
 
 
-<!-- <APPROVED> -->
+        <!-- <APPROVED> -->
         <div class="mb-3 mt-5 mx-0 row">
-            <div  style="width:50%;">APPROVED AS TO FORM:</div>
-            <div  style="width:50%;">FOR THE COURT</div>
+            <div style="width:50%;">APPROVED AS TO FORM:</div>
+            <div style="width:50%;">BY THE COURT</div>
         </div>
 
-<!-- <Parties Signature> -->
-        <div  class="m-0 row print-block" v-for="party,inx in signingPartyList" :key="'party-sign-'+inx">
+        <!-- <Parties Signature> -->
+        <div class="m-0 row print-block" v-for="party,inx in signingPartyList" :key="'party-sign-'+inx">
             <div style="width:50%;">
-                <div style="height:3rem;" />                               
-                <div style="border-top:1px dashed grey; width:94%; " > 
-                    {{party.name}} 
+                <div style="height:3rem;" />
+                <div style="border-top:1px dashed grey; width:94%; ">
+                    Signature of {{party.name}}
                     <span v-if="party.responding">, Respondent</span>
                     <span v-else>, Appellant</span>
-                </div>                               
+                </div>
             </div>
             <div v-if="inx==0" style="width:50%;">
                 <div style="height:3rem;" />
                 <div style="border-top:1px dashed grey;">
                     Deputy Registrar
                 </div>
-            </div>            
+            </div>
         </div>
 
     </div>
